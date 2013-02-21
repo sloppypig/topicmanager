536f4c3e92216c6e72eae35508918f370aea9963
miao liu <miao.liu@nsidc.org>
2013-02-20 22:01:54 -0700

    initial check in to set the basic app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Topicmanager::Application.config.secret_token = '4d0c582ddac69ac6b42a83884141ad9db579dcf04dbd7f4595af8d464dc63f0616c0381f59c4c5cb2ed2d5f117f74c084085e547cb52d99875260348183d657e'
